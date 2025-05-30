bef5377bf83d3736dd86a0d47db55aed8d70a04b
zzwsec <zzwsec@163.com>
2025-05-30 14:22:16 +0800

    feat: create ipinfo module

--- kernel.sh
+++ kernel.sh
@@ -3,8 +3,12 @@
 # Description: This script is used to automatically install the latest Linux kernel version.
 #
 # Copyright (c) 2025 zzwsec <zzwsec@163.com>
+# Copyright (c) 2025 honeok <honeok@duck.com>
 #
-# Thanks: ght (c) 2025 honeok <honeok@duck.com>
+# References:
+# https://github.com/teddysun/across
+# https://gitlab.com/fscarmen/warp
+# https://github.com/kejilion/sh
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,6 +21,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# 当前脚本版本号
+readonly VERSION='v1 (2025.05.30)'
 
 # https://www.graalvm.org/latest/reference-manual/ruby/UTF8Locale
 if locale -a 2>/dev/null | grep -qiE -m 1 "UTF-8|utf8"; then
@@ -31,18 +38,24 @@ export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH
 _red() { printf "\033[91m%b\033[0m\n" "$*"; }
 _green() { printf "\033[92m%b\033[0m\n" "$*"; }
 _yellow() { printf "\033[93m%b\033[0m\n" "$*"; }
+_blue() { printf "\033[94m%b\033[0m\n" "$*"; }
+_cyan() { printf "\033[96m%b\033[0m\n" "$*"; }
 _err_msg() { printf "\033[41m\033[1mError\033[0m %b\n" "$*"; }
 _suc_msg() { printf "\033[42m\033[1mSuccess\033[0m %b\n" "$*"; }
 _info_msg() { printf "\033[43m\033[1mInfo\033[0m %b\n" "$*"; }
 
-# shellcheck disable=SC2034
 # 各变量默认值
-GITHUB_PROXY='https://files.m.daocloud.io/'
-TEMP_DIR='/tmp/kernel'
+RANDOM_CHAR="$(head /dev/urandom | tr -dc 'A-Za-z0-9' | head -c 5)"
+TEMP_DIR="/tmp/kernel_$RANDOM_CHAR"
+UA_BROWSER='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36'
+
+# curl默认参数
+declare -a CURL_OPTS=(--max-time 5 --retry 1 --retry-max-time 10)
 
 _exit() {
     local RET_CODE="$?"
     rm -rf "$TEMP_DIR" >/dev/null 2>&1
+    [ -f /etc/apt/sources.list.d/xanmod-release.list ] && rm -f /etc/apt/sources.list.d/xanmod-release.list
     exit "$RET_CODE"
 }
 
@@ -111,11 +124,23 @@ pre_check() {
     if [ -z "$BASH_VERSION" ] || [ "$(basename "$0")" = "sh" ]; then
         error_and_exit "This script needs to be run with bash, not sh!"
     fi
-    # if [ "$(cd -P -- "$(dirname -- "$0")" && pwd -P)" != "/root" ]; then
-    #     cd /root 2>/dev/null || error_and_exit "Failed to change directory, Check permissions and try again!"
-    # fi
+    if [ "$(cd -P -- "$(dirname -- "$0")" && pwd -P)" != "$TEMP_DIR" ]; then
+        cd "$TEMP_DIR" 2>/dev/null || error_and_exit "Can't access temporary working directory. Check permissions and try again."
+    fi
     if ! _is_64bit; then
         error_and_exit "Not a 64-bit system, not supported."
+    fi
+}
+
+cdn_check() {
+    # 备用 www.prologis.cn www.autodesk.com.cn www.keysight.com.cn
+    COUNTRY="$(curl --user-agent "$UA_BROWSER" -fsL "${CURL_OPTS[@]}" "http://www.qualcomm.cn/cdn-cgi/trace" | grep -i '^loc=' | cut -d'=' -f2 | grep .)"
+    if [ "$COUNTRY" != "CN" ]; then
+        GITHUB_PROXY=""
+    elif [ "$COUNTRY" = "CN" ]; then
+        curl -sL --retry 2 --connect-timeout 5 -w "%{http_code}" "https://files.m.daocloud.io/github.com/zzwsec/zzwsec/raw/master/README.md" -o /dev/null 2>/dev/null | grep -q "^200$" && GITHUB_PROXY='https://files.m.daocloud.io/' || GITHUB_PROXY='https://gh-proxy.com/'
+    else
+        GITHUB_PROXY='https://gh-proxy.com/'
     fi
 }
 
@@ -127,12 +152,13 @@ os_reboot() {
         [Yy] | "" ) ( _exists reboot && reboot ) || shutdown -r now ;;
         * ) _yellow "Reboot has been canceled"; exit 0 ;;
     esac
+    exit 0
 }
 
 os_full() {
     local -a RELEASE_REGEX RELEASE_DISTROS
-    RELEASE_REGEX=("almalinux" "alpine" "centos" "debian" "fedora" "red hat|rhel" "rocky" "ubuntu")
-    RELEASE_DISTROS=("almalinux" "alpine" "centos" "debian" "fedora" "rhel" "rocky" "ubuntu")
+    RELEASE_REGEX=("almalinux" "centos" "debian" "fedora" "red hat|rhel" "rocky" "ubuntu")
+    RELEASE_DISTROS=("almalinux" "centos" "debian" "fedora" "rhel" "rocky" "ubuntu")
 
     if [ -s /etc/os-release ]; then
         OS_INFO="$(grep -i '^PRETTY_NAME=' /etc/os-release | awk -F'=' '{print $NF}' | sed 's#"##g')"
@@ -158,6 +184,19 @@ os_version() {
     local MAIN_VER
     MAIN_VER="$(printf "%s" "$OS_INFO" | grep -oE "[0-9.]+")"
     MAJOR_VER="${MAIN_VER%%.*}"
+}
+
+show_logo() {
+    _yellow "\
+   __                           __  \xF0\x9F\x92\x80
+  / /__ ___   ____  ___  ___   / /
+ /  '_// -_) / __/ / _ \/ -_) / / 
+/_/\_\ \__/ /_/   /_//_/\__/ /_/  
+                                  "
+    _green "System   : $OS_INFO"
+    echo "$(_yellow "Version  : $VERSION") $(_cyan "\xF0\x9F\xAA\x90")"
+    _blue 'bash <(curl -sL https://github.com/zzwsec/copy/raw/main/kernel.sh)'
+    echo
 }
 
 kernel_version() {
@@ -231,10 +270,30 @@ on_bbr() {
     fi
 }
 
-rhel_install() {
-    local ELREPO_URL LATEST_VERSION RPM_NAME
+rhel_mirror() {
+    local -a MIRRORS=(
+        mirrors.aliyun.com
+        mirrors.huaweicloud.com
+        mirror.nju.edu.cn
+        mirrors.tuna.tsinghua.edu.cn
+    )
 
-    cd "$TEMP_DIR" || error_and_exit "Unable to enter temporary working directory."
+    {
+        for MIRROR in "${MIRRORS[@]}"; do
+            {
+                AVG="$(ping -c3 -q "$MIRROR" | awk -F'/' '/rtt/ {printf "%.0f", $5}')"
+                [ -n "$AVG" ] && echo "$AVG $MIRROR"
+            } 2>/dev/null &
+        done
+        wait
+    } | sort -n | head -n1 | awk '{print $2}'
+}
+
+# http://developer.aliyun.com/mirror/elrepo?spm=a2c6h.13651102.0.0.b9361b11Q0alNh
+# https://www.rockylinux.cn/notes/rocky-linux-9-nei-he-sheng-ji-zhi-6.html
+rhel_install() {
+    local ELREPO_URL LATEST_VERSION RPM_NAME BEST_MIRROR
+
     case "$MAJOR_VER" in
         7 )
             [[ ! "$(uname -m 2>/dev/null)" =~ ^(x86_64|amd64)$ ]] && error_and_exit "Current architecture: $(uname -m) is not supported."
@@ -251,13 +310,19 @@ rhel_install() {
             rm -f kernel-ml*
         ;;
         8 | 9 )
+            BEST_MIRROR="$(rhel_mirror)"
             dnf -y install epel-release
-            # 导入ELRepo GPG公钥
-            rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
-            dnf -y install "https://www.elrepo.org/elrepo-release-$MAJOR_VER.el$MAJOR_VER.elrepo.noarch.rpm"
+            rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org # 导入ELRepo GPG公钥
+            ( rpm -q elrepo-release >/dev/null 2>&1 && [ ! -f /etc/yum.repos.d/elrepo.repo ] \
+                && dnf -y reinstall "https://www.elrepo.org/elrepo-release-$MAJOR_VER.el$MAJOR_VER.elrepo.noarch.rpm" ) \
+                || dnf -y install "https://www.elrepo.org/elrepo-release-$MAJOR_VER.el$MAJOR_VER.elrepo.noarch.rpm"
+            if [[ "$COUNTRY" = "CN" && -f /etc/yum.repos.d/elrepo.repo ]]; then
+                sed -i 's/mirrorlist=/#mirrorlist=/g' /etc/yum.repos.d/elrepo.repo
+                sed -i "s#elrepo.org/linux#$BEST_MIRROR/elrepo#g" /etc/yum.repos.d/elrepo.repo
+            fi
             dnf -y install --nogpgcheck --enablerepo=elrepo-kernel kernel-ml kernel-ml-devel
         ;;
-        *)
+        * )
             error_and_exit "Unsupported system version."
         ;;
     esac
@@ -269,45 +334,94 @@ rhel_install() {
 rhel_menu() {
     local CHOICE KERNELS
     if echo "$KERNEL_VERSION" | grep -qi 'elrepo'; then
-        _green 'ELRepo kernel detected.'
+        _green "ELRepo kernel detected."
         echo "Current kernel: $KERNEL_VERSION"
         echo
-        _yellow 'Kernel Management'
+        _yellow "Kernel Management"
         separator
-        echo '1. Update ELRepo kernel'
-        echo '2. Uninstall ELRepo kernel'
+        echo "1. Update ELRepo kernel"
+        echo "2. Uninstall ELRepo kernel"
         separator
-        reading 'Enter your choice: ' CHOICE
+        reading "Enter your choice: " CHOICE
         KERNELS="$(rpm -qa | while read -r pkg; do [[ $pkg == *kernel* && $pkg == *elrepo* ]] && echo "$pkg"; done;)"
         case "$CHOICE" in
-            1) yum remove -y elrepo-release; ([ -n "$KERNELS" ] && rpm -ev --nodeps "$KERNELS"); rhel_install ;;
-            2) yum remove -y elrepo-release; ([ -n "$KERNELS" ] && rpm -ev --nodeps "$KERNELS")
-                _suc_msg "$(_green 'ELRepo kernel uninstalled. Takes effect after reboot.')"; os_reboot ;;
-            *) _red 'Invalid selection.' ;;
+            1 ) ( [ -n "$KERNELS" ] && rpm -ev --nodeps "$KERNELS" ); rhel_install ;;
+            2 ) ( [ -n "$KERNELS" ] && rpm -ev --nodeps "$KERNELS" )
+                _suc_msg "$(_green "ELRepo kernel uninstalled. Takes effect after reboot.")"; os_reboot ;;
+            * ) error_and_exit "Invalid selection." ;;
         esac
     else
         separator
-        _red 'Please back up your data. Linux kernel will be upgraded.'
-        _yellow 'Kernel upgrade may improve performance and security. Recommended for testing, use caution in production.'
+        _red "Please back up your data. Linux kernel will be upgraded."
+        echo "Kernel upgrade may improve performance and security. Recommended for testing, use caution in production."
         separator
-        reading 'Proceed with upgrade? (y/n): ' CHOICE
+        reading "Proceed with upgrade? (y/n): " CHOICE
         case "$CHOICE" in
-            [Yy]| "") rhel_install ;;
-            [Nn]) _yellow 'Cancelled by user.'; exit 0 ;;
-            *) error_and_exit 'Invalid selection' ;;
+            [Yy] | "" ) rhel_install ;;
+            [Nn] ) _yellow "Cancelled by user."; exit 0 ;;
+            * ) error_and_exit "Invalid selection" ;;
         esac
     fi
 }
 
-kernel() {
+debian_xanmod_install() {
+    local XANMOD_VERSION
+
+    pkg_install gnupg
+    curl -fsL "${GITHUB_PROXY}github.com/kejilion/sh/raw/main/archive.key" | gpg --dearmor -o /usr/share/keyrings/xanmod-archive-keyring.gpg --yes
+    # 添加存储库
+    echo 'deb [signed-by=/usr/share/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' | tee /etc/apt/sources.list.d/xanmod-release.list
+    XANMOD_VERSION="$(curl -fsL "${GITHUB_PROXY}github.com/kejilion/sh/raw/main/check_x86-64_psabi.sh" | awk -f - | awk -F 'x86-64-v' '{print $2+0}')"
+    pkg_install "linux-xanmod-x64v$XANMOD_VERSION"
+    on_bbr
+    os_reboot
+}
+
+debian_xanmod_menu() {
+    local CHOICE
+
+    if dpkg -l | grep -q 'linux-xanmod'; then
+        _green "XanMod BBRv3 kernel detected."
+        echo "Current kernel: $KERNEL_VERSION"
+        separator
+        echo "1. Update BBRv3 kernel"
+        echo "2. Uninstall BBRv3 kernel"
+        separator
+        reading "Enter your choice: " CHOICE
+        case "$CHOICE" in
+            1 | "" ) pkg_uninstall 'linux-*xanmod1*'; update-grub; debian_xanmod_install ;;
+            2 ) pkg_uninstall 'linux-*xanmod1*'; update-grub; os_reboot ;;
+            * ) error_and_exit "Invalid selection" ;;
+        esac
+    else
+        _red "Please back up your data. XanMod BBR3 kernel will be upgraded."
+        echo "Only supports Debian/Ubuntu and only supports x86_64 architecture."
+        separator
+        reading "Proceed with upgrade? (Y/n): " CHOICE
+        case "$CHOICE" in
+            [Yy] | "" ) debian_xanmod_install ;;
+            [Nn] ) _yellow "Cancelled by user."; exit 0 ;;
+            * ) error_and_exit "Invalid selection" ;;
+        esac
+    fi
+}
+
+before_run() {
     clrscr
     pre_check
+    cdn_check
     os_full
     os_version
+    show_logo
     kernel_version
     os_check
     swap_check
+}
+
+kernel() {
+    before_run
     [[ "$OS_NAME" =~ ^(almalinux|centos|fedora|rhel|rocky)$ ]] && rhel_menu
+    [[ "$OS_NAME" =~ ^(debian|ubuntu)$ ]] && debian_xanmod_menu
 }
 
 kernel
